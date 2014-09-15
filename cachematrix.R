0e84c006b417133d1f472d8ea3c6143cd49b98d2
Thierryvar <piot-thierry@orange.fr>
2014-09-15 23:43:26 +0200

    test of modification 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Created on 15th 09/2014
+## 
 
 ## Write a short comment describing this function
+##
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
